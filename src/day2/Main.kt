e6d531fe790be789398d3254eb46b7faa367bed2
Marc Salles <marc.salles@triptease.com>
2024-12-02 23:26:24 +0100

    small refactorings on day2 code

--- src/day2/Main.kt
+++ src/day2/Main.kt
@@ -9,10 +9,28 @@ fun part1(input: List<String>): Int {
     }.count {it}
 }
 
+fun part2(input: List<String>): Int {
+    return input.map { it.split("\\s+".toRegex()).map {it.toInt()}
+    }.map {
+        val ascending = it[1] > it[0]
+        var i = 0
+        var failure = false
+        while (i<(it.size-1) && !failure) {
+            val currentIncrease = it[i+1] - it[i]
+            failure = ((!ascending || currentIncrease !in 1..3) && (ascending || currentIncrease !in -3..-1))
+            i++
+        }
+        if (failure) {
+            failure = tryWithDampener(i, failure, it)
+        }
+        !failure
+    }.count {it}
+}
+
 private fun checkIfFailurePart1(it: List<Int>): Boolean {
     val ascending = it[1] > it[0]
     var i = 0
-    var failure = false;
+    var failure = false
     while (i < (it.size - 1) && !failure) {
         val currentIncrease = it[i + 1] - it[i]
         failure = ((!ascending || !(currentIncrease in 1..3)) && (ascending || !(currentIncrease  in -3..-1)))
@@ -21,37 +39,26 @@ private fun checkIfFailurePart1(it: List<Int>): Boolean {
     return failure
 }
 
-fun part2(input: List<String>): Int {
-    return input.map { it.split("\\s+".toRegex()).map {it.toInt()}
-    }.map {
-        val ascending = it[1] > it[0]
-        var i = 0
-        var failure = false
-        while (i<(it.size-1)) {
-            val currentIncrease = it[i+1] - it[i]
-            if ((!ascending || !(currentIncrease in 1..3)) && (ascending || !(currentIncrease  in -3..-1))) {
-                var j = if (i == 0) 0 else i-1;
-                var salvageable = false;
-                while(j<=i+1) {
-                    val list = buildList() {
-                        addAll(it.subList(0,j))
-                        addAll(it.subList(j+1,it.size))
-                    }
-                    if (!checkIfFailurePart1(list)) {
-                        salvageable = true;
-                        break;
-                    }
-                    j++;
-                }
-                if (!salvageable) {
-                    failure=true;
-                }
-                break;
-            }
-            i++
+private fun tryWithDampener(i: Int, failure: Boolean, it: List<Int>): Boolean {
+    var i1 = i
+    var failure1 = failure
+    i1--
+    var j = if (i1 == 0) 0 else i1 - 1
+    while (j <= i1 + 1 && failure1) {
+        val list = removeElement(it, j)
+        if (!checkIfFailurePart1(list)) {
+            failure1 = false
         }
-        !failure
-    }.count {it}
+        j++
+    }
+    return failure1
+}
+
+private fun removeElement(it: List<Int>, j: Int): List<Int> {
+    return buildList() {
+        addAll(it.subList(0, j))
+        addAll(it.subList(j + 1, it.size))
+    }
 }
 
 fun main() {
